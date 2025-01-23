88d2c38bd29f31130c36576eac8513972d7d386c
hashimk2002 <H.M.Khan1@newcastle.ac.uk>
2025-01-23 12:48:18 +0000

    Create Heatmap_Allo24.sh

---
+++ scripts/Heatmap_Allo24.sh
@@ -0,0 +1,7 @@
+HTMAP=assay(rld)[Top20_24$ensembl_gene_id,]
+colnames(HTMAP)=row.names(sample_table)
+row.names(HTMAP)=Top20_24$external_gene_name
+myDivergePal=brewer.pal(7,"BrBG")
+pheatmap(HTMAP, color=mypalette, fontsize_row = 6,number_color = "black" ,annotation_col = heatmap_annotation,display_numbers = TRUE, annotation_colors = mat_colors, scale="none",annotation_legend = TRUE, main = "Regular Log Transformed Fold Change for the top 20 genes")
+pheatmap(HTMAP, color=myDivergePal, fontsize_row = 6,number_color = "black" ,annotation_col = heatmap_annotation,display_numbers = TRUE, annotation_colors = mat_colors, scale="row",annotation_legend = TRUE, main = "Regular Log Transformed Fold Change for the top 20 genes - Within Gene Z scores")
+pheatmap(HTMAP, color=myDivergePal, fontsize_row = 6,number_color = "black" ,annotation_col = heatmap_annotation,display_numbers = TRUE, annotation_colors = mat_colors, scale="column",annotation_legend = TRUE, main = "Regular Log Transformed Fold Change for the top 20 genes")
