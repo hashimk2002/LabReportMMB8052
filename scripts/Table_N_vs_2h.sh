f60b8b0b089d2bc96ab3b2e96f009020c642bb9a
hashimk2002 <H.M.Khan1@newcastle.ac.uk>
2025-01-24 09:18:06 +0000

    Update Table_N_vs_2h.sh
    documentation

--- scripts/Table_N_vs_2h.sh
+++ scripts/Table_N_vs_2h.sh
@@ -1,6 +1,8 @@
+#takes data from the deseq database with the aim of comparing Allo2h and Naive into a results table#
 results_tableNvs2 = results(dds, contrast= c('Group', 'Allo2h', 'Naive'))
-summary(results_tableNvs2)
+#This is converted into a tibble for further functionality as a data frame and rownames are set according to the gen ids#
 results_tibbleNvs2 = as_tibble(results_tableNvs2, rownames='ensembl_gene_id')
+#The filtered table is produced with the missing values managed by complete.cases and then extra categories added demonstrate the level of significance#
 filtered_resultsNvs2 = filter(results_tibbleNvs2, complete.cases(results_tibbleNvs2))
 filtered_resultsNvs2 = mutate(filtered_resultsNvs2, logPVal = -log10(padj))
 filtered_resultsNvs2 = mutate(filtered_resultsNvs2, Significant = padj<0.05)
