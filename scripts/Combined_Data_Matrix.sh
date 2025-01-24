3278f3904353db4db85152c907018cb2dc621731
hashimk2002 <H.M.Khan1@newcastle.ac.uk>
2025-01-24 08:46:02 +0000

    Update Combined_Data_Matrix.sh
    documentation

--- scripts/Combined_Data_Matrix.sh
+++ scripts/Combined_Data_Matrix.sh
@@ -1,3 +1,4 @@
+#txi Combines the gene-specific data i.e. abundance, count and length data found in the quant file and the location described in 'files' into a matrix with the gene mapping information
 txi = tximport(files, 
                type='salmon',
                tx2gene=gene_map,
