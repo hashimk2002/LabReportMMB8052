379d2dd7649557a2c4253dd0c333084bf0ed9a55
hashimk2002 <H.M.Khan1@newcastle.ac.uk>
2025-01-24 08:30:37 +0000

    Update Install_and_Load.sh
    Commenting

--- scripts/Install_and_Load.sh
+++ scripts/Install_and_Load.sh
@@ -1,8 +1,14 @@
+#Biomanager is a Bioconductor package which faciltates the downloading of the bioconductor packages needed for analysis#
 install.packages('BiocManager')
 library(BiocManager)
 install(c('tximport', 'DESeq2', 'biomaRt', 'pheatmap'))
+#tximport is a bioconductor package which will import the relevant transcript data and summarise it into a matrix for further gene level analysis#
 library(tximport)
+#DESeq2 is a bioconductor package which performs normalisation, dispersion estimation and determines the differential expression of genes#
 library(DESeq2)
+#biomaRt is a bioconductor package that enables the interfacing with public databases in this case its used for the ensembl gene ids#
 library(biomaRt)
+#pheatmap makes heatmaps#
 library(pheatmap)
+#tidyverse allows for table manipulation and the use of ggplot for graphs #
 library(tidyverse)
