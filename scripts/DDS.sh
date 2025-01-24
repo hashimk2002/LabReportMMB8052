81496560a9907c778091d70a2b77ae7127cc2bfb
hashimk2002 <H.M.Khan1@newcastle.ac.uk>
2025-01-24 08:54:25 +0000

    Update DDS.sh
    Documentation

--- scripts/DDS.sh
+++ scripts/DDS.sh
@@ -1,3 +1,6 @@
+#DESeqDataset from TXImport creates a DEeq Data set(dds)which is designed for comparison based on Group i.e. Naive, Allo2h and Allo24h, from the txi matrix made by Combined_Data_matrix.sh# 
 dds = DESeqDataSetFromTximport(txi, colData = sample_table, design = ~ Group)
+#DESeq performs normalisation using oth median of ratios and per-gene average transcript length as well as dispersion estimation#
 dds=DESeq(dds)
+#plotDispEsts, plots the dispersion estimates displaying max likelihood, fitted line and shrinkage estimation as described in figure 1#
 plotDispEsts(dds)
